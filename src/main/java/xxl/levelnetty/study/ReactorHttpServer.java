ac9493c19fed4d0c81832d069c9eab8668f1f0b1
zhangliangbo <2472110501@qq.com>
2021-07-24 16:46:40 +0800

    t-[]-[升级版本]-zlb

--- src/main/java/xxl/levelnetty/study/ReactorHttpServer.java
+++ src/main/java/xxl/levelnetty/study/ReactorHttpServer.java
@@ -1,20 +1,18 @@
 package xxl.levelnetty.study;
 
 
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.util.concurrent.DefaultThreadFactory;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.netty.DisposableServer;
 import reactor.netty.http.server.HttpServer;
+import reactor.netty.resources.LoopResources;
 
 @Slf4j
 public class ReactorHttpServer {
     public static void main(String[] args) {
         DisposableServer disposableServer = HttpServer.create()
-                .host("0.0.0.0")
                 .port(5556)
-                .runOn(new NioEventLoopGroup(16, new DefaultThreadFactory("civic")))
+                .runOn(LoopResources.create("civic", 1, 2, false))
                 .accessLog(true)
                 .handle((req, res) -> {
                     try {
